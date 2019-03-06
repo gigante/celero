bd58def4dbd2ae44f512e0a91d506e2f6178253e
Hiarison Gigante <gigantedesousa@gmail.com>
2019-03-06 07:53:15 -0300

    add admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models.account import Account
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class MyAdminSite(admin.AdminSite):
+    site_header = 'Celero'
+    site_url = None
+    index_title = 'Celero'
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['username', 'full_name', 'last_login']
+    ordering = ['username']
+
+    def full_name(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
+
+    full_name.short_description = 'nome'
+
+
+class AccountModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'currency', 'flow']
+    ordering = ['name', 'currency']
+
+
+admin_site = MyAdminSite(name='myadmin')
+admin_site.register(Account, AccountModelAdmin)
+admin_site.register(User, UserModelAdmin)
--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from .core.admin import admin_site
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_jwt.views import (
@@ -18,7 +18,7 @@ schema_view = get_swagger_view(title='Worksheet API')
 
 urlpatterns = [
     # django admin
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     # drf
     path('api/', include(router.urls)),
     # token
@@ -26,5 +26,5 @@ urlpatterns = [
     path('api/token/update/', refresh_jwt_token),
     path('api/token/check/', verify_jwt_token),
     # api docs (swagger)
-    path('api/docs/', schema_view),
+    path('', schema_view),
 ]
